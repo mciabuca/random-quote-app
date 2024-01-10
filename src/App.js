204bd737dff40ae63322af769c4f990a34161421
Michael Ciabuca <other404@pm.me>
2024-01-10 15:39:25 +0200

    Update styles and add X icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 function App() {
@@ -21,7 +23,9 @@ function App() {
       <p id="text">{quote}</p>
       <p id="author">- {author}</p>
       <button id="new-quote" onClick={fetchQuote}>New Quote</button>
-      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(quote + " - " + author)}`} target="_blank" rel="noopener noreferrer">Tweet Quote</a>
+      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(quote + " - " + author)}`} target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faXTwitter} beat style={{color: "#ffffff",}} size="3x"/>
+      </a>
     </div>
   );
 }
